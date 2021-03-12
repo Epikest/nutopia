8b9ffb74fffac6f622a6e20a1da4da93de9995ab
Epik <epik.est@pm.me>
2021-03-12 15:39:08 +0000

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -21,7 +21,6 @@ window.fakeStorage = {
 function LocalStorageManager() {
   this.bestScoreKey     = "bestScore";
   this.gameStateKey     = "gameState";
-  this.bestTimeKey      = "bestTime";
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
@@ -29,9 +28,9 @@ function LocalStorageManager() {
 
 LocalStorageManager.prototype.localStorageSupported = function () {
   var testKey = "test";
-  var storage = window.localStorage;
 
   try {
+    var storage = window.localStorage;
     storage.setItem(testKey, "1");
     storage.removeItem(testKey);
     return true;
@@ -47,14 +46,6 @@ LocalStorageManager.prototype.getBestScore = function () {
 
 LocalStorageManager.prototype.setBestScore = function (score) {
   this.storage.setItem(this.bestScoreKey, score);
-};
-
-LocalStorageManager.prototype.getBestTime = function () {
-  return this.storage.getItem(this.bestTimeKey) || "00:00:00";
-};
-
-LocalStorageManager.prototype.setBestTime = function (bestTime) {
-  this.storage.setItem(this.bestTimeKey, bestTime);
 };
 
 // Game state getters/setters and clearing
